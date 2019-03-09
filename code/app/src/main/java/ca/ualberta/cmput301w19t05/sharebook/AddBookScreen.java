1643d554ca0853642ad3754924ac7df9f3d5c23b
wshi <wshii1224@gmail.com>
2019-03-09 10:19:30 -0700

    test

--- code/app/src/main/java/ca/ualberta/cmput301w19t05/sharebook/AddBookScreen.java
+++ code/app/src/main/java/ca/ualberta/cmput301w19t05/sharebook/AddBookScreen.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+/**
+ * AddBookScreen
+ *      Activity for book information gathering
+ *
+ */
 public class AddBookScreen extends AppCompatActivity {
 
     @Override
