fdf8f8078d76e9e139f8ad866e8a10aa80d8a553
wshi <wshii1224@gmail.com>
2019-03-09 13:04:30 -0700

    comments

--- code/app/src/main/java/ca/ualberta/cmput301w19t05/sharebook/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/cmput301w19t05/sharebook/MainActivity.java
@@ -39,6 +39,10 @@ import ca.ualberta.cmput301w19t05.sharebook.fragments.BorrowingFragment;
 import ca.ualberta.cmput301w19t05.sharebook.fragments.MyShelfFragment;
 import ca.ualberta.cmput301w19t05.sharebook.fragments.NotificationFragment;
 
+/**
+ * MainActivity
+ *      the homepage shown after a successful login
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -86,6 +90,7 @@ public class MainActivity extends AppCompatActivity
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -105,13 +110,11 @@ public class MainActivity extends AppCompatActivity
         });
 
 
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
 
         drawerNavigationView = findViewById(R.id.nav_view);
         drawerNavigationView.setNavigationItemSelectedListener(this);
@@ -120,16 +123,13 @@ public class MainActivity extends AppCompatActivity
         bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         initFragment();
-
-
-
     }
+
 
     private void initDrawer() {
         headerView = drawerNavigationView.getHeaderView(0);
-
-
     }
+
 
     @Override
     protected void onStart() {
